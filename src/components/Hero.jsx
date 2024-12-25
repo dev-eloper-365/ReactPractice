532eae35884e8151959d7676e6a355b58e21e1f9
Dev <khero365@gmail.com>
2024-12-25 11:19:45 +0530

    Started Project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+    return <main className="hero">
+        <div className="hero-content"></div>
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+            YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+        </p>
+        <div className="hero-image"></div>
+    </main>
+};
+
+export default HeroSection;
